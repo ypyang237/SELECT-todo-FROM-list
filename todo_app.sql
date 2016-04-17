d8a31c8d555ee299dc4f4703d58c4f0016ba7103
Pamela Yang <ypyang237@gmail.com>
2016-04-16 17:00:21 -1000

    creates a table named tasks

--- todo_app.sql
+++ todo_app.sql
@@ -1,2 +1,13 @@
 DROP USER IF EXISTS micheal;
 CREATE ROLE micheal with ENCRYPTED PASSWORD 'stonebreaker';
+DROP DATABASE IF EXISTS todo_app;
+CREATE DATABASE todo_app;
+\c todo_app
+CREATE TABLE tasks (
+  id SERIAL NOT NULL PRIMARY KEY,
+  title varchar(255) NOT NULL,
+  description text,
+  created_at timestamp NOT NULL DEFAULT now(),
+  updated_at timestamp,
+  completed boolean NOT NULL
+);
