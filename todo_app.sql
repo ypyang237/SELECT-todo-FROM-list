417266e18ebb1d47c3c0768dd5305cbfeb1faa79
Pamela Yang <ypyang237@gmail.com>
2016-04-16 17:09:09 -1000

    finishes

--- todo_app.sql
+++ todo_app.sql
@@ -16,3 +16,36 @@ ALTER TABLE tasks DROP COLUMN completed;
 ALTER TABLE tasks ADD COLUMN completed_at timestamp NULL;
 ALTER TABLE tasks ALTER COLUMN updated_at SET NOT NULL;
 ALTER TABLE tasks ALTER COLUMN updated_at SET Default now();
+
+INSERT INTO tasks
+  (title, description, created_at, updated_at, completed_at)
+  VALUES ('Study SQL', 'Complete this exercise', now(), now(), NULL);
+INSERT INTO tasks
+  (title, description)
+  VALUES ('Study PostgreSQL', 'Read all the documentation');
+
+SELECT title FROM tasks;
+UPDATE tasks SET completed_at = now() WHERE title = 'Study SQL';
+SELECT title, description FROM tasks WHERE completed_at is NULL;
+
+SELECT * FROM tasks Order By created_at;
+
+INSERT INTO tasks
+  (title, description)
+  VALUES ('mistake 1', 'a test entry');
+INSERT INTO tasks
+  (title, description)
+  VALUES ('mistake 2', 'another test entry');
+INSERT INTO tasks
+  (title, description)
+  VALUES ('third mistake', 'another test entry');
+
+SELECT title FROM tasks WHERE title LIKE '%mistake%';
+DELETE FROM tasks WHERE title = 'mistake 1';
+SELECT title, description FROM tasks WHERE title LIKE '%mistake%';
+DELETE FROM tasks WHERE title LIKE '%mistake%';
+
+SELECT * FROM tasks Order By id;
+
+
+-- SELECT * FROM tasks;
